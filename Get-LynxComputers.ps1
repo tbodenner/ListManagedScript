1f7bddd724fefdfae367f4c25dc91ff13b14b8ce
tbodenner <204929239+tbodenner@users.noreply.github.com>
2026-01-26 08:35:47 -0700

    Added switch and updated dates in Get-LynxComputers.ps1.

--- Get-LynxComputers.ps1
+++ Get-LynxComputers.ps1
@@ -1,16 +1,29 @@
-# check if today is monday
-if ((Get-Date).DayOfWeek -eq 'Monday') {
-    # if it is monday, get all computers
+param (
+    # switch to get all computers
+    [switch]$All
+)
+# check our switch
+if ($All -eq $true) {
     .\Get-AllADComputers.ps1 -OutFile .\Lynx\ComputerList.txt -Filter $Global:MyComputerFilter -RootOU $Global:MyRootOU
     Write-Host "List created with all computers." -ForegroundColor Cyan
 }
 else {
-    # otherwise, get any computers added in the last two days
-    $Date = (Get-Date).AddDays(-2)
+    # check if today is monday
+    if ((Get-Date).DayOfWeek -eq 'Monday') {
+        # otherwise, get any computers added in the last seven days
+        $Date = (Get-Date).AddDays(-7)
+        Write-Host "Appending list with computers added in last SEVEN days." -ForegroundColor Cyan
+    }
+    else {
+        # otherwise, get any computers added in the last two days
+        $Date = (Get-Date).AddDays(-2)
+        Write-Host "Appending list with computers added in last TWO days." -ForegroundColor Cyan
+    }
+    # update our computer list using our date
     .\Get-AllADComputers.ps1 -OutFile .\Lynx\ComputerList.txt `
-                             -Filter $Global:MyComputerFilter `
-                             -RootOU $Global:MyRootOU `
-                             -AfterDate $Date `
-                             -Append
+                        -Filter $Global:MyComputerFilter `
+                        -RootOU $Global:MyRootOU `
+                        -AfterDate $Date `
+                        -Append
     Write-Host "Computer list appended." -ForegroundColor Green
 }
